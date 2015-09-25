0e86d3a37f03e49ba56c3ab930dc4507662d9b5d
yyh <yanie8899@gmail.com>
2015-09-24 21:45:40 -0400

    Second commit

--- js/script.js
+++ js/script.js
@@ -5,29 +5,36 @@ $(function(){
 	   pause: true
 	});
     
-    //check the state of menuOne since it is in default expand when loading
-	if ($('div[arialabelledby="menuOne"]').hasClass("in")==true){
-      		$('#menuOne a.heading').css('color','#e75640');     		
-   		}else{
-   			$('#menuOne a.heading').css('color','#fff');
-   	};
-	
-	//OnClick event to change the heading color and arrow color and direction
-	//Need to set the time out because need to wait for the class added after click
-	var myfunc = function(){
-	   		if ($('div[arialabelledby="menuOne"]').hasClass("in")==true){
-	      			$('#menuOne a.heading').css('color','#e75640');
-	      			$('head').append("<style>#menuOne a.heading::after{ content:'▼'; float: 'right';}</style>");
-	   		}else{
-	   				$('#menuOne a.heading').css('color','#fff');
-	   				$('head').append("<style>#menuOne a.heading::after{ content:'▶'; float: 'right';}</style>");   			
-	   		};
-	};
+    //Set up the correct color for menuOne to start with. The default is in expand state
+	$('#menuOne a.heading').css('color','#e75640');
+   
+    //on click event to toggle the color and arrow
+	$("a.heading").click(function(){
+		    //find the parent id and find the first class
+            var thisID = $(this).closest("div").attr("id"),  
+	   		    thisClass = $(this).attr("class"),
+	   		    firstClass = thisClass.substr(0, thisClass.indexOf(" "))||thisClass;
+    	
+    		//Set timeout to wait for the jQuery style toggle in, then reset the color and arrow
+            setTimeout(function(){  
+            	var myclass='#'+thisID +' .'+firstClass;
+			    if ($("#"+thisID).next().hasClass("in")==true){ //if expanding
+		      			$(myclass).css('color','#e75640');		      		
+		      			$(myclass).find('span').remove();
+		      			$(myclass).append("<span class='glyphicon glyphicon-triangle-bottom'></span>");
+		   		}else{//collapse
+		   				$(myclass).css('color','#fff');
+		   				$(myclass).find('span').remove();
+		   				$(myclass).append("<span class='glyphicon glyphicon-triangle-right'></span>");   			
+		   		}
+		   		//check the rest of menu to reset according
+		   		if ($('a.heading').hasClass('collapsed')==true){
+		   				$('a.heading').not(myclass).css('color','#fff');
+		   				$('a.heading').not(myclass).find('span').remove();
+		   				$('a.heading').not(myclass).append("<span class='glyphicon glyphicon-triangle-right'></span>"); 
+		   		}
 
-	$("#menuOne a.heading").click(function(){
-	   setTimeout(myfunc,500);
-   	});
-		
-	
+	   },500);
+   	})	
 });
 
